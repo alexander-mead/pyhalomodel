9be767124f319ab2041ea62308c1d498616f9cd7
Alexander Mead <alexander.j.mead@googlemail.com>
2023-01-04 16:01:50 +0000

    derived constants

--- modules/constants.py
+++ modules/constants.py
@@ -1,27 +1,14 @@
+from math import pi
+
 # Physical constants
-h = 6.62607004e-34 # Planck constant [kg m^2 s^-1]
-c = 2.998e8 # Speed of light [m s^-1]
-kB = 1.38064852e-23 # Boltzmann constant [kg m^2 s^-2 K^-1]
-mp = 1.6726219e-27 # Proton mass [kg]
-
-# Units
-Jy = 1e-26 # Jansky [W m^-2 Hz^-1]
-au = 1.496e11 # au [m]
-eV_mass = 1.79e-36 # eV/c^2 [kg]
-year = 365.25*24.*60.*60. # year [s]
-
-# Time
-seconds_in_day = 60*60*24
+G = 6.6743e-11 # Newton constant [m^3 kg^-1 s^-2]
 
 # Astronomy
-R_Sun = 6.957e8 # Solar radius [m]
-M_Sun = 2.e30 # Solar mass [kg]
+Sun_mass = 1.9884e30 # Mass of the Sun [kg]
+Mpc = 3.0857e16*1e6  # Mpc [m]
 
 # Cosmology
-# TODO: Derive those that should be derived
-Hdist = 2997.92458 # c/H0 [Mpc/h]
-Htime = 9.777922217 # 1/H0 [Gyr/h]
-H0 = 100. # H0 in h [km/s/Mpc]
-#rhoc = 277537383080.52332 # Critical density [(Msun/h) (Mpc/h)^-3]
-rhoc = 2.775373e11 # Critical density [(Msun/h) (Mpc/h)^-3]
-nuconst = 93.1 # Neutrino mass required to close universe [eV]
+H0 = 100.                       # H0 in h [km/s/Mpc]
+G_cos = G*Sun_mass/(Mpc*1e3**2) # Gravitational constant [(Msun/h)^-1 (km/s)^2 (Mpc/h)] ~4.301e-9 (1e3**2 m -> km)
+rhoc = 3.*H0**2/(8.*pi*G_cos)   # Critical density [(Msun/h) (Mpc/h)^-3] ~2.775e11 (Msun/h)/(Mpc/h)^3
+nuconst = 93.1                  # Neutrino mass required to close universe [eV]
